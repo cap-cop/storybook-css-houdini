211fc1cbcfcfa466c96ad8756fdd815721646af5
nopdam <niels.opdam@capgemini.com>
2022-07-17 14:53:27 +0200

    initial commit

---
+++ src/stories/Page.stories.ts
@@ -0,0 +1,53 @@
+import { moduleMetadata, Story, Meta } from '@storybook/angular';
+import { within, userEvent } from '@storybook/testing-library';
+import { CommonModule } from '@angular/common';
+import {useEffect} from '@storybook/addons';
+import Button from './button.component';
+import Header from './header.component';
+import Page from './page.component';
+
+import { expect } from '@storybook/jest';
+
+export default {
+  title: 'Example/Page',
+  component: Page,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/angular/configure/story-layout
+    layout: 'fullscreen',
+  },
+  decorators: [
+    moduleMetadata({
+      declarations: [Button, Header],
+      imports: [CommonModule],
+    })
+  ],
+} as Meta;
+
+const Template: Story<Page> = (args: Page) => ({
+  props: args,
+});
+
+export const LoggedOut = Template.bind({});
+
+// More on interaction testing: https://storybook.js.org/docs/angular/writing-tests/interaction-testing
+export const LoggedIn = Template.bind({});
+LoggedIn.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  const loginButton = await canvas.getByRole('button', { name: /Log in/i });
+  await userEvent.click(loginButton);
+
+  await expect(
+    canvas.getByText(
+      'Log out'
+    )
+  ).toBeTruthy();
+
+  const logoutButton = await canvas.getByRole('button', { name: /Log out/i });
+  await userEvent.click(logoutButton);
+  await expect(
+    canvas.getByText(
+      'Log in'
+    )
+  ).toBeTruthy();
+
+};
